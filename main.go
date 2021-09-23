9561300294c7ca65d00d75fbd1c8cd97ae9f743b
arttur <artur.poetsch@ynov.com>
2021-09-23 16:16:14 +0200

    folie

--- main.go
+++ main.go
@@ -6,40 +6,42 @@ import (
 )
 
 func main() {
+	// fonction qui execute nos sous fonctions et rentre les valeur ainsi que le menu principal
 	var p1 Personnage
 	p1.Init("Matéo", "elfe", 1, 100, 30, []string{"Potion de vie", "Potion de vie", "Potion de vie"}, []string{"Coup de poing"})
 	p1.spellbook("Feu")
 	fmt.Println(p1.skill)
-	// var menu int
-	// fmt.Println("+++++++++++++++++++++++++++++++")
-	// fmt.Println("A quoi voulez vous accéder:")
-	// time.Sleep(1 * time.Second)
-	// fmt.Println("----- \n Afficher les informations du personnage (1)")
-	// time.Sleep(1 * time.Second)
-	// fmt.Println("----- \n Accéder au contenu de l’inventaire (2)")
-	// time.Sleep(1 * time.Second)
-	// fmt.Println("----- \n Voir le Marchand (3)")
-	// time.Sleep(1 * time.Second)
-	// fmt.Println("----- \n Quitter (4) \n-----")
-	// time.Sleep(1 * time.Second)
-	// fmt.Println("Entrez le numéro de l'option:")
-	// fmt.Println("+++++++++++++++++++++++++++++++")
-	// fmt.Scanln(&menu)
-	// switch menu {
-	// case 1:
-	// 	p1.DisplayInfo()
-	// 	p1.Dead()
-	// case 2:
-	// 	p1.AccessInventory()
-	// case 3:
-	// 	p1.Marchand()
-	// case 4:
-	// 	fmt.Println("Fin de la transmission")
-	// 	break
-	// }
+	var menu int
+	fmt.Println("+++++++++++++++++++++++++++++++")
+	fmt.Println("A quoi voulez vous accéder:")
+	time.Sleep(1 * time.Second)
+	fmt.Println("----- \n Afficher les informations du personnage (1)")
+	time.Sleep(1 * time.Second)
+	fmt.Println("----- \n Accéder au contenu de l’inventaire (2)")
+	time.Sleep(1 * time.Second)
+	fmt.Println("----- \n Voir le Marchand (3)")
+	time.Sleep(1 * time.Second)
+	fmt.Println("----- \n Quitter (4) \n-----")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Entrez le numéro de l'option:")
+	fmt.Println("+++++++++++++++++++++++++++++++")
+	fmt.Scanln(&menu)
+	switch menu {
+	case 1:
+		p1.DisplayInfo()
+		p1.Dead()
+	case 2:
+		p1.AccessInventory()
+	case 3:
+		p1.Marchand()
+	case 4:
+		fmt.Println("Fin de la transmission")
+		break
+	}
 }
 
 type Personnage struct {
+	// creation de la structure de notre personnage
 	name      string
 	class     string
 	level     int
@@ -50,6 +52,7 @@ type Personnage struct {
 }
 
 func (p *Personnage) Init(name string, class string, level int, lpmax int, lp int, inventory []string, skill []string) {
+	// initialisation de notre personnage
 	p.name = name
 	p.class = class
 	p.level = level
@@ -60,6 +63,7 @@ func (p *Personnage) Init(name string, class string, level int, lpmax int, lp in
 }
 
 func (p *Personnage) AccessInventory() {
+	// fonction qui nous permet d'acceder a notre inventaire
 	var rep int
 	if len(p.inventory) == 0 {
 		fmt.Println("inventaire vide fraté")
@@ -76,6 +80,7 @@ func (p *Personnage) AccessInventory() {
 }
 
 func retour() {
+	// fonction qui nous permet de retourner au menu précédent
 	var rep int
 	fmt.Println("tapez 1 pour retourner zo menu précédent")
 	fmt.Scanln(&rep)
@@ -85,6 +90,7 @@ func retour() {
 }
 
 func (p *Personnage) spellbook(item string) {
+	// fonction qui nous permet d'ajouter ou repertorier les sorts (spell)
 	h := &p.skill
 	for _, letter := range p.skill {
 		if letter == ("Boule de feu") {
@@ -97,6 +103,7 @@ func (p *Personnage) spellbook(item string) {
 }
 
 func (p *Personnage) Marchand() {
+	// fonction affichant le menu du marchand , et les ajoute a notre inventaire
 	var menum int
 	fmt.Println("-----------------Marchand-------------------")
 	fmt.Println("tapez 1 pour obtenir une Potion de vie ;)")
@@ -120,6 +127,7 @@ func (p *Personnage) Marchand() {
 }
 
 func (p Personnage) DisplayInfo() {
+	// fonction nous permettant de voir les informations de notre personnage
 	fmt.Println("-----------")
 	fmt.Println("Nom:", p.name)
 	fmt.Println("Classe:", p.class)
@@ -138,6 +146,7 @@ func (p *Personnage) AddInventory(item string) {
 }
 
 func (p *Personnage) TakePot() {
+	// fonction qui nous permet de prendre une potion de soin
 	for _, letter := range p.inventory {
 		if letter == "Potion de vie" {
 			if p.lp <= (p.lpmax - 50) {
@@ -157,6 +166,7 @@ func (p *Personnage) TakePot() {
 }
 
 func (p *Personnage) Dead() {
+	// fonction qui verifie si le personnage est mort et le ressussite a la moitié de ses pv
 	if p.lp == 0 {
 		fmt.Printf("Bravo, vous êtes mort. \n")
 		time.Sleep(2 * time.Second)
@@ -170,6 +180,7 @@ func (p *Personnage) Dead() {
 }
 
 func (p *Personnage) PoisonPot() {
+	// fonction qui crée la potion poison et explique ce qu'elle fait sur un personnage
 	for _, letter := range p.inventory {
 		if letter == "Potion de poison" {
 			time.Sleep(1 * time.Second)
